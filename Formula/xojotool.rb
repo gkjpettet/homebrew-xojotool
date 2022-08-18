ed991a6e6040f5132e6cd3a4b6accd78eb709c89
Garry Pettet <contact@garrypettet.com>
2022-08-18 16:56:52 +0100

    Version 1.0.5

--- Formula/xojotool.rb
+++ Formula/xojotool.rb
@@ -1,8 +1,8 @@
 class Xojotool < Formula
   desc "A command line tool for working with Xojo projects."
   homepage "https://github.com/gkjpettet/xojotool"
-  url "https://github.com/gkjpettet/homebrew-xojotool/files/9374705/xojotool-1.0.4-macos.zip"
-  sha256 "71d2655302b712ac60a730722b619ccf4ce0f7296f7480f7f8847b74e52b494d" # openssl sha256 [file]
+  url "https://github.com/gkjpettet/homebrew-xojotool/files/9375423/xojotool-1.0.5-macos.zip"
+  sha256 "d7bdf454cf8d222117a171795bc6a7743451275a1e5e911bfdff2c9746328ceb" # openssl sha256 [file]
 
   def install
 	bin.install "xojotool Libs"
