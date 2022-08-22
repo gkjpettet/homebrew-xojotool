03f1404194db1d722388918eb5583623235db8df
Garry Pettet <contact@garrypettet.com>
2022-08-22 08:55:48 +0100

    Added support for parsing MobileScreen items in iOS projects.

--- Formula/xojotool.rb
+++ Formula/xojotool.rb
@@ -1,8 +1,8 @@
 class Xojotool < Formula
   desc "A command line tool for working with Xojo projects."
   homepage "https://github.com/gkjpettet/xojotool"
-  url "https://github.com/gkjpettet/homebrew-xojotool/files/9375423/xojotool-1.0.5-macos.zip"
-  sha256 "d7bdf454cf8d222117a171795bc6a7743451275a1e5e911bfdff2c9746328ceb" # openssl sha256 [file]
+  url "https://github.com/gkjpettet/homebrew-xojotool/files/9375423/xojotool-1.0.6-macos.zip"
+  sha256 "4867ce8370ecd7194b0fadd2fe962595b5efd080494fda2132b68fe0f94600b4" # openssl sha256 [file]
 
   def install
 	bin.install "xojotool Libs"
